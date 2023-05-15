3557ce80bb2c562bc35e88fc0ce30217b8346e81
taygun <kara.taygun@gmail.com>
2023-05-15 09:32:30 +0300

    java faker

--- src/test/java/com/spotify/oauth2/tests/PlaylistTests.java
+++ src/test/java/com/spotify/oauth2/tests/PlaylistTests.java
@@ -5,70 +5,52 @@ import com.spotify.oauth2.models.Error;
 import com.spotify.oauth2.models.Playlist;
 import com.spotify.oauth2.utils.ConfigLoader;
 import com.spotify.oauth2.utils.DataLoader;
+import com.spotify.oauth2.utils.FakerUtils;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static com.spotify.oauth2.api.TokenManager.RenewToken;
 import static com.spotify.oauth2.api.applicationApi.PlaylistHelper.*;
+import static com.spotify.oauth2.utils.FakerUtils.generatePlaylistDescription;
+import static com.spotify.oauth2.utils.FakerUtils.generatePlaylistName;
 
 public class PlaylistTests {
-
     @Test
     public void ShouldBeAbleToCreateAPlaylist(){
-
-        Playlist requestPlaylist = playlistBuilder("New playlist", "New playlist description", false);
-
+        Playlist requestPlaylist = playlistBuilder(generatePlaylistName(), generatePlaylistDescription(), false);
         Response response = new PlaylistApi().post(requestPlaylist, RenewToken());
         assertStatusCode(response, 201);
-
         Playlist responsePlaylist = response.as(Playlist.class);
         assertPlaylistEqual(responsePlaylist, requestPlaylist);
     }
     @Test
     public void ShouldBeAbleToGetPlaylist(){
-
         Playlist requestPlaylist = playlistBuilder("New Playlist", "New playlist description", false);
-
         Response response = new PlaylistApi().get(DataLoader.getInstance().getGetPlaylistID(), RenewToken());
         assertStatusCode(response, 200);
-
         Playlist responsePlaylist = response.as(Playlist.class);
         assertPlaylistEqual(responsePlaylist, requestPlaylist);
-
-
     }
     @Test
     public void ShouldBeAbleToUpdate(){
-
-        Playlist requestPlaylist = playlistBuilder("Updated Playlist Name", "Updated playlist description", false);
-
+        Playlist requestPlaylist = playlistBuilder(generatePlaylistName(), generatePlaylistDescription(), false);
         Response response = new PlaylistApi().update(requestPlaylist, DataLoader.getInstance().getUpdatePlaylistID(), RenewToken());
         assertStatusCode(response, 200);
-
     }
     @Test
     public void ShouldNotBeAbleToCreateAPlaylistWithName(){
-
-        Playlist requestPlaylist = playlistBuilder("", "Playlist description", false);
-
+        Playlist requestPlaylist = playlistBuilder("", generatePlaylistDescription(), false);
         Response response = new PlaylistApi().post(requestPlaylist, RenewToken());
         assertStatusCode(response, 400);
-
         Error error = response.as(Error.class);
         assertError(error, 400, "Missing required field: name");
-
     }
     @Test
     public void ShouldNotBeAbleToCreateAPlaylistWithExpiredToken(){
-
-        Playlist requestPlaylist = playlistBuilder("Playlist Name", "Playlist description", false);
-
+        Playlist requestPlaylist = playlistBuilder(generatePlaylistName(), generatePlaylistDescription(), false);
         Response response = new PlaylistApi().post(requestPlaylist, ConfigLoader.getInstance().getInvalidToken());
         assertStatusCode(response, 401);
-
         Error error = response.as(Error.class);
         assertError(error, 401, "Invalid access token");
-
     }
-
 }
---
+++ src/test/java/com/spotify/oauth2/utils/FakerUtils.java
@@ -0,0 +1,14 @@
+package com.spotify.oauth2.utils;
+
+import com.github.javafaker.Faker;
+
+public class FakerUtils {
+    public static String generatePlaylistName(){
+        Faker faker = new Faker();
+        return "Playlist " + faker.regexify("[A-Za-z0-9, _-]{10}");
+    }
+    public static String generatePlaylistDescription(){
+        Faker faker = new Faker();
+        return "Description " + faker.regexify("[A-Za-z0-9, _-]{10}");
+    }
+}
